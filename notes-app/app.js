c786c7019fea9024e38459cd5779d9c7e626f7ed
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-05-12 19:43:04 +0300

    11. importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,13 @@
+const validator = require('validator')
 const note = require('./notes.js')
 
 const message = note()
 console.log(message)
 
+console.log(validator.isEmail('example.com'))
+console.log(validator.isEmail('nasos@example.com'))
+
+console.log(validator.isURL('sgadsg'))
+console.log(validator.isURL('https://www.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728848?start=90#overview'))
 
 
