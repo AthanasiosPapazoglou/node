b9ac9952783df1a20e33f2408404cdb1a5b86815
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-05-13 18:54:52 +0300

    12. color print

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,6 @@
+const chalk = require('chalk')
 const validator = require('validator')
+
 const note = require('./notes.js')
 
 const message = note()
@@ -10,4 +12,4 @@ console.log(validator.isEmail('nasos@example.com'))
 console.log(validator.isURL('sgadsg'))
 console.log(validator.isURL('https://www.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728848?start=90#overview'))
 
-
+console.log(chalk.blue('Ultra Top Kek'))
