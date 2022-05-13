21aa8b86682cc469ac340a97fabefa856f4c4fe6
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-05-13 19:32:27 +0300

    15 mid

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,10 +6,6 @@ const note = require('./notes.js')
 const message = note()
 console.log(message)
 
-console.log(validator.isEmail('example.com'))
-console.log(validator.isEmail('nasos@example.com'))
+console.log(process.argv)
 
-console.log(validator.isURL('sgadsg'))
-console.log(validator.isURL('https://www.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728848?start=90#overview'))
-
-console.log(chalk.blue('Ultra Top Kek'))
+console.log(chalk.green.inverse('ligma'))
